8bd008940110d01b16c5098b89e764d69b74f364
aturtledude <turtledude.mkt@gmail.com>
2022-08-31 23:15:22 +0200

    Fix actions path

--- optimizer.py
+++ optimizer.py
@@ -31,7 +31,7 @@ def optimize(workDir, inventoryLines, tickets, playerLevel):
     coverageFile = os.path.join(workDir, "data", "alldata.json")
     actionsFile = os.path.join(workDir, "data", "actions.csv")
     courses, items = reader.readJson(coverageFile)
-    reader.readActions(base.ACTIONS_FILE, courses)
+    reader.readActions(actionsFile, courses)
     inventory = reader.readInventory(inventoryLines, items)
     result = ""
     result += "The inventory has " + str(len(inventory.drivers)) + " drivers"
