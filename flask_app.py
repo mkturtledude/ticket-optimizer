dac4cfa096e4a7ca3d72d206564caf5d52ac469f
aturtledude <turtledude.mkt@gmail.com>
2024-05-15 09:34:33 +0200

    Update for mii tour

--- flask_app.py
+++ flask_app.py
@@ -16,8 +16,7 @@ from wtforms import IntegerField, FileField, SelectField, TextAreaField, Boolean
 import base, util, reader
 
 pastTours = [
-    ("current", "Bowser"),
-    ("17-mii.json", "Mii"),
+    ("current", "Mii"),
     ("18-princess.json", "Princess"),
     ("19-mario-vs-luigi.json", "Mario vs. Luigi"),
     ("20-night.json", "Night"),
@@ -42,11 +41,10 @@ pastTours = [
     ("13-ninja.json", "Ninja"),
     ("14-yoshi.json", "Yoshi"),
     ("15-spring.json", "Spring"),
+    ("16-bowser.json", "Bowser"),
 ]
 
 rankedWeeks = [
-    ("16-bowser.json1", "Bowser 1"),
-    ("16-bowser.json2", "Bowser 2"),
     ("17-mii.json1", "Mii 1"),
     ("17-mii.json2", "Mii 2"),
     ("18-princess.json1", "Princess 1"),
@@ -97,6 +95,8 @@ rankedWeeks = [
     ("14-yoshi.json2", "Yoshi 2"),
     ("15-spring.json1", "Spring 1"),
     ("15-spring.json2", "Spring 2"),
+    ("16-bowser.json1", "Bowser 1"),
+    ("16-bowser.json2", "Bowser 2"),
 ]
 
 class MultiCheckboxField(SelectMultipleField):
